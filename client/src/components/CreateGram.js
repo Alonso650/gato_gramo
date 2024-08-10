82a06776cea941489d4a2c94cb0437e876a58b8d
Hector Alonzo-Torres <hectoralonzotorres@Hectors-MacBook-Pro.local>
2024-08-09 18:56:05 -0700

    adopt form fully works now need to fix stray issue and maybe model db

--- client/src/components/CreateGram.js
+++ client/src/components/CreateGram.js
@@ -4,98 +4,95 @@ import axios from "axios";
 import { useForm } from "react-hook-form";
 import { validationSchema } from "../helpers/schema"
 import { yupResolver } from "@hookform/resolvers/yup"
-import { MultistepForm } from "./MultistepForm";
-import { GramForm } from './GramForm';
-import { ShelterForm } from './ShelterForm';
-import { StrayForm } from './StrayForm'
-import { HouseForm } from './HouseForm';
-import styles from './CreateGram.module.css';
+import styles from './CreateGram.module.css'
+
+const MAX_STEPS = 3;
 
 
 function CreateGram(){
   const navigate = useNavigate();
-  //const [isAdopt, setIsAdopt] = useState(false);
-  const [data, setData] = useState({
-    title: "",
-    gramText: "",
-    image: "",
-    adoptInfoStreet: "",
-    adoptInfoCity: "",
-    adoptInfoState: "", 
-    adoptInfoZipcode: 0,
-    isFromShelter: false,
-    isAdopt: false,
-    isStray: false,
-    // adding this flag for the last part of the adoption from
-    isFromHome: false,
-  })
+  // const [isAdopt, setIsAdopt] = useState(false);
+  // const [isFromShelter, setIsFromShelter] = useState(false);
+  // const [isStray, setIsStray] = useState(false);
+  const [formStep, setFormStep] = useState(0);
+  const { watch, register, handleSubmit, formState:{errors}} = useForm({
+    defaultValues:{
+      title: "",
+      gramText: "",
+      image: "",
+      isAdopt: "",
+      isFromShelter: "",
+      adoptInfoGender: "",
+      IsStray: "",
+      adoptInfoCatType: "",
+      adoptInfoStreet: "",
+      adoptInfoCity: "",
+      adoptInfoState: "",
+      adoptInfoZipcode: "",
+    },
+    resolver: yupResolver(validationSchema),
+  });
 
+  const watchedIsAdopt = watch("isAdopt");
+  const watchedIsStray = watch("isStray");
+  const watchedIsFromShelter = watch("isFromShelter");
 
-
-
-  const updateFields = (fields) => {
-    setData(prev => {
-      return { ...prev, ...fields};
-    });
-  };
-
-  // const updateIsAdopt = (value) => {
-  //   setData((prev) => ({
-  //     ...prev,
-  //     isAdopt: value,
-  //   }));
-  // };
-
-  
-  const { currentStepIndex, step, steps, isFirstStep, isLastStep, next, back } = MultistepForm([
-    <GramForm {...data} updateFields={updateFields} />,
-    <ShelterForm {...data} updateFields={updateFields} isAdopt={data.isAdopt} isFromShelter={data.isFromShelter} />,
-    <StrayForm {...data} updateFields={updateFields} isStray={data.isStray}/>,
-    <HouseForm {...data} updateFields={updateFields} isFromHome={data.isFromHome} />
-  ])
-
-  console.log("Succes from isfromhome");
-  const submitForm = (e) => {
-    // prevents the default form submission behavior allowing the form
-    // to go to next
-    e.preventDefault();
-    if(!isLastStep){
-      if(currentStepIndex === 1 && data.isAdopt === false){
-        alert("Gram created since cat not looking for a new home");
-        console.log("data in the if statement: ", data);
-      } else {
-        alert("went to next");
-        return next();
-      }
+  const completeFormStep = () => {
+    if(formStep < 2){
+      setFormStep(cur => cur + 1);
+      console.log("step: " + (formStep + 1));
     }
-    
-    console.log("Submitting form...");
-    console.log("FormData: ", data);
-    const formData = new FormData();
-    // console.log(imageData);
-    console.log(data.title);
-    console.log(typeof data.isAdopt);
+  }
+  const handleNextButton = (e) => {
+    e.preventDefault();
+    completeFormStep();
+  }
+
+  // refractor into its own component
+  const renderButton = () => {
+    if(formStep > 2){
+      return undefined;
+    } else if (formStep === 2 || (formStep === 1 && watchedIsAdopt === "false")){
+      return (
+        <button type="submit">Create Gram!</button>
+      )
+    } else {
+      return (
+        <button type="button" onClick={handleNextButton}>Next</button>
+      )
+    }
+  }
+
+  const goToPrevStep = () => {
+    setFormStep(cur => cur - 1);
+  }
+
+  const submitForm = (data) => {
+
+    if(formStep === 2 || (formStep === 1 && data.isAdopt === "false")){
+      const formData = new FormData();
     
     /* Since I have named the file input as "image", to access the information
        I have to specify the name so data.image[0] allows access to the file.
        Then include all information in a formdata
 
     */
-    formData.append("image", data.image);
+
+    formData.append("image", data.image[0]);
     formData.append("gramText", data.gramText);
     formData.append("title", data.title);
+    // adoption info
     formData.append("isAdopt", data.isAdopt);
-    formData.append("isStray", data.isStray);
     formData.append("isFromShelter", data.isFromShelter);
-    formData.append("isFromHome", data.isFromHome);
-    formData.append("adoptInfoStreet", data.adoptInfoStreet);
+    formData.append("adoptInfoGender", data.adoptInfoGender);
+    formData.append("adoptInfoCatType", data.adoptInfoCatType);
+    formData.append("isStray", data.isStray);
+    formData.append("adoptInfoStreet", data.adoptInfoStreet); 
     formData.append("adoptInfoCity", data.adoptInfoCity);
     formData.append("adoptInfoState", data.adoptInfoState);
     formData.append("adoptInfoZipcode", data.adoptInfoZipcode);
+    
 
-    console.log("Form data before axios: ", formData);
-    
-    
     axios.post("http://localhost:3001/grams", formData,{ 
       headers: {
         accessToken: localStorage.getItem("accessToken"),
@@ -107,35 +104,151 @@ function CreateGram(){
       })
       .catch((err) =>alert("File upload error: " + err.message))
 
-    alert("Success creation after api call");
+    completeFormStep();
+    }
     
   };
 
+  /*
+    NOTES: 
+    FIX POSITIONING OF THE FORM SINCE ITS TOO LONG AND GOES INTO THE NAVBAR
+
+  */
+
   return(
-    <div className={styles.createGramPage}>
-     <form className={styles.formContainer} 
-        onSubmit ={submitForm}>
-        <div style={{ position: "absolute", top: ".5rem", right: ".5rem" }}>
-          {/* {currentStepIndex + 1} / {steps.length} */}
-        </div>
-        {step}
-        <div style={{
-              marginTop: "1rem",
-              display: "flext",
-              gap: ".5rem",
-              justifyContent: "flex-end",
-            }}
-        >
-          {!isFirstStep && (
-            <button type="button" onClick={back}>
-              Back
-            </button>
+    <div className='createGramPage'>
+     <form className="formContainer" 
+        onSubmit ={handleSubmit(submitForm)}>
+        {formStep < MAX_STEPS && (
+          <div>
+            {formStep > 0 && (
+              <button onClick={goToPrevStep} type="button">Back</button>
+            )}
+            <p>
+              Step {formStep + 1} of {MAX_STEPS}
+            </p>
+          </div>
+        )}
+        {formStep >= 0 && (
+          <section className={formStep === 0 ? styles.block : styles.none}>
+          <h1>Gram Creation</h1>  
+  
+          {/* <label>Title:</label> */}
+          <input
+            id="title"
+            name="title"
+            placeholder="Title"
+            {...register('title')}
+            /> 
+  
+           {/* <label>Description:</label> */}
+          <input
+            id="gramText"
+            type="text"
+            placeholder="Description"
+            name="gramText"
+            {...register('gramText')}
+          />
+  
+          {/* <label>Image:</label> */}
+          <div>
+            <label>Image:</label>
+            <input
+              type="file"
+              name="image"
+              {...register("image")}
+              id="image"
+            />
+          </div>
+          </section>
+        )}
+        
+        {formStep >= 1 && (
+          <section className={formStep === 1 ? styles.block : styles.none}>
+          <label>Is this cat looking for a new home?</label>
+          <div>
+            <select
+              {...register('isAdopt')}
+              // onChange={handleAdoptChange}
+            >
+              <option value="">Select</option>
+              <option value="true">Yes</option>
+              <option value="false">No</option>
+            </select>
+          </div>
+
+          {watchedIsAdopt === "true" &&(
+            <>
+            <label>Is This cat located at a shelter?</label>
+            <div>
+              <select
+                {...register('isFromShelter')}
+                // onChange={handleShelterChange}
+              >
+                <option value="">Select</option>
+                <option value="true">Yes</option>
+                <option value="false">No</option>
+              </select>
+            </div>
+
+            <label> Is This a stray cat? </label>
+            <div>
+              <input
+                type="checkbox"
+                id="isStray"
+                {...register('isStray')}
+                // onChange={handleStrayChange}
+              />
+            </div>
+            </>
           )}
-        {/* <button type="submit">{isLastStep ? (data.isAdopt === false ? "Create Gram" : "Submit") : "Next"}</button> */}
-        <button type="submit">
-          { (data.isFromShelter || data.isStray || data.isFromHome) ? "Submit" : "Next"}
-        </button>        
-        </div>
+          </section>
+        )}
+
+
+        {formStep >= 2 && (
+          <section className={formStep === 2 ? styles.block : styles.none}>
+          <h4>Enter approximate location of the cat: (Note: personal address won't show up on gram posts)</h4>
+          <input
+            id="adoptInfoStreet"
+            placeholder="Enter street address"
+            name="adoptInfoStreet"
+            {...register("adoptInfoStreet")}
+          />
+
+          <input
+            id="adoptInfoCity"
+            placeholder="Enter the City"
+            name="adoptInfoCity"
+            {...register("adoptInfoCity")}
+          />
+
+          <input
+            id="adoptInfoState"
+            placeholder="Enter state"
+            name="adoptInfoState"
+            {...register("adoptInfoState")}
+          />
+
+          <input
+            id="adoptInfoZipcode"
+            placeholder="Enter Zipcode"
+            name="adoptInfoZipcode"
+            {...register("adoptInfoZipcode")}
+          />
+          </section>
+        )}
+ 
+        {formStep === 3 && (
+          <section>
+            <h2>Gram Created!</h2>
+          </section>
+        )}
+        
+        {renderButton()}
+        <pre>
+          {JSON.stringify(watch(), null, 2)}
+          </pre>        
       </form>
     </div>
   )
--- server/models/Grams.js
+++ server/models/Grams.js
@@ -1,3 +1,70 @@
+// module.exports = (sequelize, DataTypes) => {
+//     const Grams = sequelize.define("Grams", {
+//         title: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//         },
+//         gramText:{
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//         },
+//         image:{
+//             type: DataTypes.STRING,
+//         },
+//         imageId:{
+//             type: DataTypes.STRING,
+//         },
+//         username:{
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//         },
+//         // adding adoption info
+//         isAdopt:{
+//             type: DataTypes.BOOLEAN,
+//         },
+//         // adoptInfoGender:{
+//         //     type: DataTypes.STRING,
+//         //     allowNull: true,
+//         // },
+       
+//         // adoptInfoCatType:{
+//         //     type: DataTypes.STRING,
+//         //     allowNull: true,
+//         // },
+//         isStray:{
+//             type: DataTypes.BOOLEAN,
+//             allowNull: true,
+//         },
+//         //  // split up the location 
+//         isFromShelter:{
+//             type: DataTypes.BOOLEAN,
+//             allowNull: true,
+//         },
+//         // isFromHome:{
+//         //     type: DataTypes.BOOLEAN,
+//         //     allowNull: true,
+//         // },
+//         adopInfoStreet:{
+//             type: DataTypes.STRING,
+//             allowNull: true,
+//         },
+//          adoptInfoCity:{
+//             type: DataTypes.STRING,
+//             allowNull: true,
+//         },
+//         adoptInfoState:{
+//             type: DataTypes.STRING,
+//             allowNull: true,
+//         },
+//         adoptInfoZipcode:{
+//             type: DataTypes.INTEGER,
+//             allowNull: true,
+//         },
+//     });
+    
+//     return Grams;
+// };
+
 module.exports = (sequelize, DataTypes) => {
     const Grams = sequelize.define("Grams", {
         title: {
@@ -22,43 +89,31 @@ module.exports = (sequelize, DataTypes) => {
         isAdopt:{
             type: DataTypes.BOOLEAN,
         },
-        // adoptInfoGender:{
-        //     type: DataTypes.STRING,
-        //     allowNull: true,
-        // },
-       
-        // adoptInfoCatType:{
-        //     type: DataTypes.STRING,
-        //     allowNull: true,
-        // },
-        isStray:{
-            type: DataTypes.BOOLEAN,
-            allowNull: true,
+        adoptInfoGender:{
+            type: DataTypes.STRING,
         },
-        //  // split up the location 
+       
+        adoptInfoCatType:{
+            type: DataTypes.STRING,
+        },
+        IsStray:{
+            type: DataTypes.BOOLEAN,
+        },
+         // split up the location 
         isFromShelter:{
             type: DataTypes.BOOLEAN,
-            allowNull: true,
-        },
-        isFromHome:{
-            type: DataTypes.BOOLEAN,
-            allowNull: true,
         },
         adopInfoStreet:{
-            type: DataTypes.STRING,
-            allowNull: true,
+            type: DataTypes.STRING
         },
          adoptInfoCity:{
             type: DataTypes.STRING,
-            allowNull: true,
         },
         adoptInfoState:{
             type: DataTypes.STRING,
-            allowNull: true,
         },
         adoptInfoZipcode:{
-            type: DataTypes.INTEGER,
-            allowNull: true,
+            type: DataTypes.STRING,
         },
     });
     
